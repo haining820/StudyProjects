2972417c7414b4ecc9b9aac26f19f6edcb6b3287
haining820 <hn0820@qq.com>
2022-07-24 23:22:43 +0800

    add futureTask

---
+++ future-study/src/main/java/qunar/futuretask/FutTaskDemo.java
@@ -0,0 +1,37 @@
+package qunar.futuretask;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import qunar.Task;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * User: yuhaining
+ * Date: 2022-07-24
+ * Time: 22:01
+ */
+public class FutTaskDemo {
+
+    private Logger LOGGER = Logger.getLogger(FutTaskDemo.class);
+
+    @Test
+    public void testDuTask() throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<>(new FuTaskCallable());
+
+        LOGGER.info("isDone1->" + futureTask.isDone());
+        new Thread(futureTask).start();
+        LOGGER.info(futureTask.get());
+
+        TimeUnit.SECONDS.sleep(10);
+
+        LOGGER.info("isDone2->" + futureTask.isDone());
+        new Thread(futureTask).start();
+        LOGGER.info(futureTask.get());
+    }
+
+}
