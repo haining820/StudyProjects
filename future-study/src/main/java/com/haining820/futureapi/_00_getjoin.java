f7d7b4b9a36ac57865a47f50359f34372acdf69a
haining820 <hn0820@qq.com>
2023-01-13 01:15:23 +0800

    update

--- future-study/src/main/java/com/haining820/futureapi/_00_getjoin.java
+++ future-study/src/main/java/com/haining820/futureapi/_00_getjoin.java
@@ -1,5 +1,9 @@
 package com.haining820.futureapi;
 
+import com.haining820.utils.SmallTool;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -12,35 +16,61 @@ import java.util.concurrent.TimeoutException;
  * Date: 2022-09-19
  * Time: 17:34
  */
+@Slf4j
 public class _00_getjoin {
 
+    @Test
     public void getTest() throws ExecutionException, InterruptedException {
         CompletableFuture<Integer> future = new CompletableFuture<>();
         Integer integer = future.get();
-        int i = 3;
-        System.out.println(i);  //  future中没有内容，会一直阻塞，不会输出结果
+        // future中没有内容，会一直阻塞，不会输出结果
+        log.info("1");
     }
 
+    @Test
     public void timeoutGetTest() throws ExecutionException, InterruptedException {
         CompletableFuture<Integer> future = new CompletableFuture<>();
         try {
-            Integer integer = future.get(5, TimeUnit.SECONDS);  // 超过规定的5s就会抛出超时异常
+            // 超过规定的5s就会抛出超时异常
+            Integer integer = future.get(5, TimeUnit.SECONDS);
         } catch (TimeoutException e) {
             e.printStackTrace();
         }
-        int i = 3;
-        System.out.println(i);
+        log.info("3");
     }
 
+    @Test
     public void getNowTest() {
-        CompletableFuture<Integer> cf = new CompletableFuture<>();
-        Integer i = cf.getNow(666);
-        System.out.println("cf->" + i);    // cf没有结果，输出默认结果666
-
+        CompletableFuture<Integer> cf1 = new CompletableFuture<>();
+        // cf没有结果，打印 666
+        log.info("cf getNow res#={}", cf1.getNow(666));
         CompletableFuture<Integer> cf2 = new CompletableFuture<>();
         cf2.complete(777);
-        Integer i2 = cf2.getNow(666);
-        System.out.println("cf2->"+i2);    // cf有结果，直接输出结果，输出777
+        // cf有结果，直接输出结果，打印 777
+        log.info("cf get res#={}", cf2.getNow(666));
+    }
+
+    @Test
+    public void getNowTest2() {
+        CompletableFuture<Integer> getNowCf = CompletableFuture.supplyAsync(() -> sleepMillis(1000));
+        // sleepMillis操作需要等待，没有结果立即返回，打印 666
+        log.info("cf getNow res#={}", getNowCf.getNow(666));
+        CompletableFuture<Integer> getCf = CompletableFuture.supplyAsync(() -> sleepMillis(1000));
+        try {
+            // 等待sleepMillis结果完成，打印 1
+            log.info("cf get res#={}", getCf.get());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Integer sleepMillis(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return 1;
     }
 
 }
